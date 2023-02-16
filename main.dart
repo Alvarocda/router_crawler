e6a21b6a9d623ba3e7d71dce661454e63f6097e2
Álvaro Claro <alvaro.claro@hotmail.com>
2023-02-15 20:44:13 -0400

    performance optmizations

--- main.dart
+++ main.dart
@@ -3,40 +3,40 @@ import 'dart:io';
 void main() async {
   print('hello World');
   
-  String results = await getInfo(directory: Directory('./results'), stringBuffer: StringBuffer());
-  results.replaceAll('-', '\n');
+  String results = await getInfo(directory: Directory('./results'));
   print(results);
 }
 
-Future<String> getInfo({required Directory directory, required StringBuffer stringBuffer}) async {
+///
+///
+///
+Future<String> getInfo({required Directory directory}) async {
   final List<FileSystemEntity> folders = await directory.list().toList();
+  StringBuffer stringBuffer = StringBuffer();
 
   if(folders.whereType<Directory>().isEmpty){
-    List<String> folderNames = directory.path.split('/');
-    return getIp(folderNames);
+    List<String> foldersNames = directory.path.split('/');
+    return getIp(foldersNames);
   }
-
   for (int x = 0; x < folders.whereType<Directory>().toList().length; x++) {
       Directory nextDir = Directory(folders[x].path);
-      await getInfo(directory: nextDir, stringBuffer: stringBuffer);
+      stringBuffer.write(await getInfo(directory: nextDir));
   }
-  
-  print(stringBuffer.toString());
-
   return stringBuffer.toString();
 }
 
-
-String getIp(List<String> foldersName){
+///
+///
+///
+String getIp(List<String> foldersNames){
   StringBuffer ip = StringBuffer();
   for(int x = 4; x > 0; x--){
     if(x == 1){
-      ip.writeln(foldersName[foldersName.length - x]);
+      ip.writeln(foldersNames[foldersNames.length - x]);
     } else {
-      ip.write('${foldersName[foldersName.length - x]}.');
+      ip.write('${foldersNames[foldersNames.length - x]}.');
     }
     
   }
-  print(ip.toString());
   return ip.toString();
 }
